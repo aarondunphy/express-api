95289cd8adff5c0aeea67613a1cd3cb8ace78379
Aaron Dunphy <aaronjohndunphy@gmail.com>
2023-10-03 15:57:24 +0100

    Typescript added

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import "dotenv/config";
-// import "./tracing.js";
 import express from "express";
 import userRoutes from "./routes/users.js";
 
 const app = express();
 
-app.listen(3000);
+app.listen(3000, () => console.log("App running on http://localhost:3000"));
 
-app.get("/", (req, res) => res.json({ title: "Hello World from express 1" }));
+app.get("/", (req, res) => res.json({ title: "Hello World from express 1!" }));
 
 app.use("/users", userRoutes);
