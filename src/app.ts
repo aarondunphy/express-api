e40bce838dae527b2c54bd6d29fb847ba28d6186
Aaron Dunphy <aaronjohndunphy@gmail.com>
2023-10-03 19:54:55 +0100

    lets goooo

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,6 @@ import userRoutes from "./routes/users.js";
 
 export const app = express();
 
-app.get("/", (_req, res) => res.json({ title: "Zip function uploaded!!!" }));
+app.get("/", (_req, res) => res.json({ title: "WE HAVE A WORKING CI/CD" }));
 
 app.use("/users", userRoutes);
